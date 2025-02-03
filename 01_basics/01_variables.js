40c83591c655c43d6ddd61320cb448b3c468779b
Jitendra Berde <berde.jitendra14@gmail.com>
2025-02-03 14:01:37 +0530

    Variables

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -1,1 +1,13 @@
 console.log("Tejashree Berde")
+const accountId = 1234;
+let accountEmail = "test@gmail.com";
+var accountPassword = "abcd";
+accountCity = "Mumbai";
+
+//accountId = 2; // not allowed
+accountEmail = "Tejashree@gmail.com";
+accountPassword = "pqrs";
+accountCity = "Jaipur";
+
+console.log(accountId);
+console.table([accountId, accountEmail,accountPassword,accountCity]);
