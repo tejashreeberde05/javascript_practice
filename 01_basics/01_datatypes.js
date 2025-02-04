d22c012defe3375c4682d5aefa59580fe1ae257a
Jitendra Berde <berde.jitendra14@gmail.com>
2025-02-04 22:19:00 +0530

    Datatypes

--- 01_basics/01_datatypes.js
+++ 01_basics/01_datatypes.js
@@ -19,12 +19,12 @@ console.log(fruit);
 let country = "India";
 console.log(country);
 
+
+
 /*
 JavaScript Number
-
 Integers - Numeric values without any decimal parts. Example: 3, -74, etc.
 Floating-Point - Numeric values with decimal parts. Example: 3.15, -1.3, etc.
-
 */
 
 //integer value
@@ -35,12 +35,12 @@ let floatNumber = 1.23;
 console.log(floatNumber);
 
 
+
 /*
 JavaScript BigInt
-
 BigInt is a type of number that can represent very large or very small integers beyond the range of the regular number data type.
-
 */
+
 
 //Bigint Value
 let bigintNumber = 98764322222222n;
@@ -53,22 +53,25 @@ let resultAns = bigintNumber + 1n;
 
 console.log(resultAns);
 
+
+
+
 /*
 JavaScript Boolean
 A Boolean data can only have one of two values: true or false. 
 */
+
  let dataChecked = "true"
  console.log(dataChecked)
 
  let statusChecked = "False";
  console.log(statusChecked);
 
+
+
  /*
-
  JavaScript undefined
-
  If a variable is declared but the value is not assigned, then the value of that variable will be undefined.
-
  */
 
  let fname;
@@ -77,22 +80,25 @@ A Boolean data can only have one of two values: true or false.
  let name= undefined
  console.log(name);
 
+
+
+
  /*
  JavaScript null
  In JavaScript, null represents "no value" or "nothing." For example,
-
 */
+
 let number ="null"
 console.log(number);
 
- /*
 
+
+ /*
  JavaScript Symbol
 A Symbol is a unique and primitive value. This data type was introduced in ES6.
-
 When you create a Symbol, JavaScript guarantees that it is distinct from all other symbols, even if they have the same descriptions. 
-
 */
+
 //Rwo symbols with same description
 
 let value1 = Symbol("Javascript")
@@ -101,11 +107,12 @@ let value2 = Symbol("Javascript")
 console.log(value1 == value2);
 
 
+
 /*
 JavaScript Object
 An Object holds data in the form of key-value pairs. For example,
-
 */
+
 let studentInfo = {
             fName : "Raj",
             lName :"Nair",
