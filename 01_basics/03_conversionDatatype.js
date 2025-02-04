4b46150938694cec2a8af51718a5a725f2263478
Jitendra Berde <berde.jitendra14@gmail.com>
2025-02-04 22:57:52 +0530

    DatatypeConversion

---
+++ 01_basics/03_conversionDatatype.js
@@ -0,0 +1,21 @@
+let score = "33"
+
+console.log(typeof score)
+console.log(typeof(score));
+
+let stringtoNumber = Number(score)
+console.log(typeof stringtoNumber)
+console.log(typeof(stringtoNumber));
+
+
+let isLoggedIn = 1;
+let booleanisLoggedIn = Boolean(isLoggedIn)
+
+console.log(booleanisLoggedIn);
+
+/*
+1=> true ; 0=> false
+"" => false
+"string" => true
+*/
+
