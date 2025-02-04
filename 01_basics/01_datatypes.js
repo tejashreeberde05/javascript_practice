75f27c8ae9539bba0d0d2ba008dfba59aefc5e7d
Jitendra Berde <berde.jitendra14@gmail.com>
2025-02-04 11:42:20 +0530

    Datatypes

---
+++ 01_basics/01_datatypes.js
@@ -0,0 +1,71 @@
+"use strict";// treat all JS code as  newer version 
+// alert("hello") // we are using node.js , not browser
+
+//String	- Textual Data
+//Number    - An Integer or floating poimt number
+//BigInt	- An integer with arbitrary precision.
+//Boolean	 - Any of two values: true or false.	
+//undefined  - A data type whose variable is not initialized or value is not assigned.	
+//null      -	Denotes a null value.	
+//Symbol	- A data type whose instances are unique and immutable.
+//Object	- Key-value pairs of collection of data.
+
+
+// string enclosed in single quotes
+let  fruit = 'Apple';
+console.log(fruit);
+
+//String enclosed in double quotes
+let country = "India";
+console.log(country);
+
+/*
+JavaScript Number
+
+Integers - Numeric values without any decimal parts. Example: 3, -74, etc.
+Floating-Point - Numeric values with decimal parts. Example: 3.15, -1.3, etc.
+
+*/
+
+//integer value
+let intNumber = 1;
+console.log(intNumber);
+
+let floatNumber = 1.23;
+console.log(floatNumber);
+
+
+/*
+JavaScript BigInt
+
+BigInt is a type of number that can represent very large or very small integers beyond the range of the regular number data type.
+
+*/
+
+//Bigint Value
+let bigintNumber = 98764322222222n;
+console.log(bigintNumber);
+
+
+// add two big intergers or numbers
+
+let resultAns = bigintNumber + 1n;
+
+console.log(resultAns);
+
+/*
+JavaScript Boolean
+A Boolean data can only have one of two values: true or false. 
+*/
+ let dataChecked = "true"
+ console.log(dataChecked)
+
+ let statusChecked = "False";
+ console.log(statusChecked);
+ 
+ 
+
+
+
+
+
