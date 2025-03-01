eac0e5b2b27e09aad37cbd7cf453943be6591e22
Jitendra Berde <tejashree.training@gmail.com>
2025-03-01 12:24:12 +0530

    Objects

---
+++ 02_basics/02_objetcs.js
@@ -0,0 +1,32 @@
+/* JavaScript Objects : JavaScript objects are collections of properties, where each property is defined as a key-value pair. They are used to model real-world entities and complex data structures.Objects are variables too. But objects can contain many values.
+  
+   Syntax: Objects can be created using either the object literal syntax {} or the Object() constructor.
+   objectName.methodName()
+
+
+   JavaScript Object Literal : An object literal is a list of name:value pairs inside curly braces {}.
+   {firstName:"John", lastName:"Doe", age:50, eyeColor:"blue"}
+
+*/
+
+/*Creating a JavaScript Object*/
+
+// Create an Object
+const person = {
+    firstName: "John",
+    lastName: "Doe",
+    age: 50,
+    eyeColor: "blue"
+  };
+  console.log(person.firstName);
+  console.log(person["lastName"]);
+  
+
+  /* This example creates an empty JavaScript object, and then adds 4 properties: */
+
+ 
+  
+  
+
+
+  
